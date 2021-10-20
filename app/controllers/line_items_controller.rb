7a9e10e16d10b6bb05285ce990fdb1a58c8f5ca8
RobertsMarkuns <djrex963@gmail.com>
2021-10-20 17:31:46 +0300

    Task E: A Smarter Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@ class LineItemsController < ApplicationController
 
   include CurrentCart
   include VisitCounter
-  
+
   before_action :set_cart, only: [:create]
   before_action :reset_counter, only: [:create]
   before_action :set_line_item, only: %i[ show edit update destroy ]
@@ -28,15 +28,17 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to @line_item.cart }
+        format.json { render :show,
+          status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -69,8 +71,9 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+    # Never trust parameters from the scary internet, only allow the white
+   # list through.
+   def line_item_params
+     params.require(:line_item).permit(:product_id)
+   end
 end
