a12db582cfdb7d7599ffc0e840a912c5859435bf
RobertsMarkuns <djrex963@gmail.com>
2021-10-20 14:33:07 +0300

    Task D: playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,13 +1,9 @@
 class StoreController < ApplicationController
-  
+
   include VisitCounter
   before_action :set_counter, only: [:index]
 
   def index
-    if params[:set_locale]
-      redirect_to store_index_url(locale: params[:set_locale])
-    else
       @products = Product.order(:title)
-    end
   end
 end
