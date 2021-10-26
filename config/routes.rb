6bf795038413989fb2eda3ce554bb485c391304a
RobertsMarkuns <djrex963@gmail.com>
2021-10-26 14:02:33 +0300

    Iteration G2: Atom Feeds

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
   resources :carts
   resources :products
   root 'store#index', as: 'store_index'
-  
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 end
