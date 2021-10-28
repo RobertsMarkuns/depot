8cb841d58547945064111d91b8c798fa89280157
RobertsMarkuns <djrex963@gmail.com>
2021-10-28 18:49:25 +0300

    Iteration J4: Adding a Sidebar, More Administration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users or /users.json
+  # GET /users
+  # GET /users.json
   def index
     @users = User.order(:name)
   end
 
-  # GET /users/1 or /users/1.json
+  # GET /users/1
+  # GET /users/1.json
   def show
   end
 
@@ -19,7 +21,8 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users or /users.json
+  # POST /users
+  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -29,13 +32,15 @@ class UsersController < ApplicationController
           notice: "User #{@user.name} was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @user.errors,
+          status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -43,19 +48,26 @@ class UsersController < ApplicationController
           notice: "User #{@user.name} was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render :edit }
+        format.json { render json: @user.errors,
+          status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1 or /users/1.json
+  # DELETE /users/1
+  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url,
+        notice: '"User #{@user.name} deleted"' }
       format.json { head :no_content }
     end
+  end
+
+  rescue_from 'User::Error' do |exception|
+    redirect_to users_url, notice: exception.message
   end
 
   private
@@ -64,7 +76,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
     end
