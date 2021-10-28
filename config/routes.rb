c3c9c5f48c4ea5872244ca09736fd5cb87086da1
RobertsMarkuns <djrex963@gmail.com>
2021-10-28 17:40:13 +0300

    Task J: Logging In

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails51 for more book information.
 #---
 Rails.application.routes.draw do
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
