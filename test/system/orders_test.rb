f10b5bf3c608dd4132d3da585c6c74af3282c8e0
RobertsMarkuns <djrex963@gmail.com>
2021-10-28 17:18:05 +0300

    Iteration I2: Connecting to a Slow Payment Processor with Active Job

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,16 +1,19 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   setup do
     @order = orders(:one)
   end
 
-  test "visiting the index" do
-    visit orders_url
-    assert_selector "h1", text: "Orders"
-  end
-
   test "check routing number" do
+
+    LineItem.delete_all
+    Order.delete_all
+
+    visit store_index_url
+
     visit store_index_url
 
     first('.catalog li').click_on 'Add to Cart'
@@ -25,8 +28,38 @@ class OrdersTest < ApplicationSystemTestCase
 
     select 'Check', from: 'pay_type'
 
+
     assert_selector "#order_routing_number"
-  end 
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",      order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check",            order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+
+  end
+
+  test "visiting the index" do
+    visit orders_url
+    assert_selector "h1", text: "Orders"
+  end
 
   test "creating a Order" do
     visit orders_url
